ae11aa62ef245462698f3ee2a468ee404979bab1
Sávio Mendes <saviomd@gmail.com>
2018-08-02 16:42:08 -0300

    Lesson 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+class MyErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error);
+    console.log(info);
+    this.setState(state => ({ ...state, hasError: true }));
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Error :(</div>
+    } else {
+      return this.props.children;
+    }
+  }
+}
+
+class Profile extends Component {
+  onClick = () => {
+    throw new Error('Oh no!');
+  }
+
+  render() {
+    return (
+      <div onClick={this.onClick}>
+        Name: {this.props.user.name}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { name: 'Chris' },
+    };
+  }
+
+  updateUser = () => {
+    this.setState(state => ({ ...state, user: null }));
+  };
+
+  render() {
+    return (
+      <div>
+        <MyErrorBoundary>
+          <Profile user={this.state.user} />
+          <button onClick={this.updateUser}>Update</button>
+        </MyErrorBoundary>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
